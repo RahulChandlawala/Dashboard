e8cbbed6c3858a31d8dd0c8c13286f02e8ce8db0
Rahul <rahulchandlawala@gmail.com>
2022-07-14 18:32:23 +0530

    Edited

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -13,7 +13,7 @@ import {
 	Edit,
 	Inject,
 } from "@syncfusion/ej2-react-grids";
-import { contextMenuItems, ordersData, ordersGrid } from "../data/dummy";
+import { ordersData, ordersGrid } from "../data/dummy";
 import { Header } from "../componets";
 
 const Orders = () => {
